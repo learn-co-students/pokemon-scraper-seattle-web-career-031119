98b511736204ba35f91d9e3561ec0fa5227ec25f
github username <github email address>
2019-03-22 16:29:11 -0700

    Done.

--- bin/run.rb
+++ bin/run.rb
@@ -4,4 +4,10 @@ Scraper.new(@db).scrape
 
 all_pokemon = @db.execute("SELECT * FROM pokemon;")
 
-# test out your code here!
+@sql_runner.execute_create_hp_column
+pikachu = Pokemon.new(id: 1, name: "Pikachu", type: "electric", db: @db)
+# magikarp = Pokemon.save('Magikarp', 'water', @db)
+pikachu.alter_hp(59, @db)
+
+binding.pry
+#0
